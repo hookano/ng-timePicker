48790334bd3f04a151f267b9f17d68afdc33f3b6
hookano <tech@hookano.ca>
2018-04-25 11:20:51 -0600

    Update ng-timePicker.js

--- js/ng-timePicker.js
+++ js/ng-timePicker.js
@@ -16,30 +16,21 @@
             var id     = 'time-' + Date.now()
             var handle = null
 
-            scope.ngModel = scope.ngModel || {
-                    hours  : 12,
-                    minutes: 0,
-                    seconds: 0
-                }
+            scope.ngModel = scope.ngModel || '12:00';
             element.addClass(id)
-            element.attr('value', '12:00:00')
-            handle = new TimePicker('.' + id)
+            element.attr('value', scope.ngModel)
+            handle = new TimePicker('.' + id) 
 
             // view to model
             handle.on('change', function () {
-                scope.ngModel.hours   = this.value.hours
-                scope.ngModel.minutes = this.value.minutes
-                scope.ngModel.seconds = this.value.seconds
-                scope.ngModel.all     = element.val()
-
+                scope.ngModel = element.val();
                 scope.$applyAsync()
             });
 
 
             // model to view
             scope.$watch('ngModel', function (newVal) {
-                handle.value = newVal
-                handle.refreshInputs()
+              element.attr('value', newVal);
             })
         }
     }
